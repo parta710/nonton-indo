69e3ee98cf31ba7787551bf11796af208bc364a4
parta710 <bagasasn2@gmail.com>
2026-05-29 13:51:05 +0700

    Create build.gradle.kts

---
+++ Donghub/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "id"
+    description = "Nonton Donghua subtitle Indonesia"
+    authors = listOf("parta710")
+    status = 1
+    tvTypes = listOf("Anime")
+    iconUrl = "https://donghub.vip/wp-content/uploads/2026/04/Donghub-Baru.png"
+}
